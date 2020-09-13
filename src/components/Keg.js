06caf6ce37f6c1f369bb6698cb5c0deaf277b1d1
ppearce05 <ppearce05@Josephs-Mac.local>
2020-09-13 15:56:48 -0700

    add styling, formatting

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -7,16 +7,19 @@ function Keg(props) {
     <React.Fragment>
       <div class="flex">
         <div class="flex-grow text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-            <h4>{props.name}</h4>
+            <h4>{props.brand}</h4>
         </div>
         <div class="flex-grow text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-            <h4>{props.brand}</h4>
+            <h4>{props.name}</h4>
         </div>
         <div class="flex-grow text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
             <h4>{props.price}</h4>
         </div>
-        <div class="flex-grow text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-            <h4>{props.abv}</h4>
+
+        <div class="flex text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+            <pourForm>
+            <pourButton>Pour</pourButton>
+            </pourForm>
         </div>
        
       </div> 
--- src/styles/index.js
+++ src/styles/index.js
@@ -14,6 +14,19 @@ const StyledContainer = styled.main.attrs( {
         ${tw`bg-white shadow max-w-xl mx-auto py-6 px-4 sm:px-6 lg:px-8 text-4xl font-bold leading-tight text-gray-900`}
         }
 
+        pourForm {
+        ${tw`flex-auto content-center bg-white shadow max-w-xl mx-auto py-6 px-4 sm:px-6 lg:px-8 text-4xl font-bold leading-tight text-gray-900`}
+          }
+
+        pourButton {
+          ${tw`bg-yellow-500 hover:bg-yellow-200 text-white font-bold py-2 px-4 border border-blue-700 rounded`}
+        }
+
+        /* pourButton {
+          ${tw`bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded mb-1`}
+        } */
+
+      
         /* form {
         ${tw`flex content-center text-center bg-white text-center rounded py-8 px-5 shadow max-w-md`}
           } */
@@ -25,6 +38,8 @@ const StyledContainer = styled.main.attrs( {
         button {
           ${tw`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-blue-700 rounded`}
         }
+
+
 
 
         
