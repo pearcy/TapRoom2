18dd3aee8ba5195c83a82cbe3dd0dad58f5d0c76
ppearce05 <ppearce05@Josephs-Mac.local>
2020-09-14 01:53:14 -0700

    add form handler - submit test to console

--- src/components/KegForm.js
+++ src/components/KegForm.js
@@ -1,20 +1,30 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import { v4 } from 'uuid';
 
 
 function KegForm(props) {
+    function handleNewTicketFormSubmission(event) {
+        event.preventDefault();
+        console.log(event.target.brand.value);
+        console.log(event.target.varietal.value)
+        console.log(event.target.price.value);
+        console.log(event.target.abv.value);
+    }
+
     return (
         <React.Fragment>
-            <form>
-                <input type="text" placeholder="Brand" />
-                <input type="text" placeholder="Varietel" />
-                <input type="text" placeholder="Price" />
-                <input type="text" placeholder="ABV" />
+            <form onSubmit={handleNewTicketFormSubmission}>
+                <input type="text" name="brand" placeholder="Brand" />
+                <input type="text" name="varietal" placeholder="Varietal" />
+                <input type="text" name="price" placeholder="Price" />
+                <input type="text" name="abv" placeholder="ABV" />
                 <button>Submit</button>
             </form>
         </React.Fragment>
     );
 }
+
 
 KegForm.propTypes = {
     brand: PropTypes.string.isRequired,
