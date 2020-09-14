0d80642f4385b4628a7a23a2f60b52f17d628a1a
ppearce05 <ppearce05@Josephs-Mac.local>
2020-09-14 08:08:52 -0700

    fix error KegList - working to this point

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -6,12 +6,11 @@ import Keg from "./Keg";
 function KegList(props) {
   return (
     <React.Fragment>
-    <hr />
      <div class="p-10">
      <header>Keg List</header> 
          <div class="border shadow rounded-lg p-10">
             <div class="max-w-xl flex-column">
-            {props.KegList.map((keg, index) => 
+            {props.kegList.map((keg, index) => 
                 <Keg 
                 brand={keg.brand}
                 varietal={keg.varietal}
